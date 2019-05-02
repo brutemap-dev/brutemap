78ef7ec22b3b13cd6c54bfae40f2aca61d919212
brutemap-dev <brutemapdev@gmail.com>
2019-05-03 06:25:33 +0700

    'simple spinner progress'

---
+++ lib/utils/progress.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Brutemap is (c) 2019 By Brutemap Development Team.
+See LICENSE for details.
+"""
+
+import itertools
+
+from lib.core import stdoutWrite
+from lib.settings import SPINNER_CHARS
+
+class Spinner(object):
+    def __init__(self, message, maxval=None):
+        self.marker = itertools.cycle(SPINNER_CHARS)
+        self.message = message
+        self.curval = 0
+        self.maxval = maxval or 100
+        self._width = 0
+
+        stdoutWrite(message)
+
+    @property
+    def percent(self):
+        return (self.curval * 100) / self.maxval
+
+    def write(self, msg):
+        backspace = "\b" * self._width
+        newmsg = backspace + msg.ljust(self._width)
+        stdoutWrite(newmsg)
+        self._width = max(self._width, len(msg))
+
+    def show_progress(self):
+        self.curval += 1
+        progress = "%d%% %s" % (self.percent, self.marker.next())
+        self.write(progress)
+
+    def done(self):
+        stdoutWrite("\b")
+        stdoutWrite("(done)\n")
