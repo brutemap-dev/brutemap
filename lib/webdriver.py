f3fc1cf83f58ceed509cf81d92c585d17d284702
brutemap-dev <brutemapdev@gmail.com>
2019-05-03 06:15:43 +0700

    'core functions for accessing webdriver'

---
+++ lib/webdriver.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""
+Brutemap is (c) 2019 By Brutemap Development Team.
+See LICENSE for details.
+"""
+
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver import Firefox
+from selenium.webdriver import Chrome
+from selenium.webdriver import WebKitGTK
+from selenium.webdriver import Edge
+from selenium.webdriver import Safari
+from selenium.webdriver import PhantomJS
+from selenium.webdriver import BlackBerry
+from selenium.webdriver import Android
+
+from lib.data import logger
+from lib.data import SETTING
+from lib.data import TARGET
+
+def initWebDriver():
+    """
+    Menemukan WebDriver yang terpasang.
+    """
+
+    infoMsg = "Finding web driver..."
+    logger.info(infoMsg)
+
+    webdriver = [Firefox, Chrome, WebKitGTK, Edge, Safari, PhantomJS, BlackBerry, Android]
+    for driver in webdriver:
+        try:
+            infoMsg = "Trying %s ..." % str(driver)
+            logger.info(infoMsg)
+
+            _browser = driver()
+            infoMsg = "WebDriver found: %s" % str(driver)
+            logger.info(infoMsg)
+
+            # simpan WebDriver, untuk digunakan :func:`reinitWebDriver`.
+            SETTING.WEB_DRIVER = driver
+            # simpan objek instan WebDriver.
+            SETTING.BROWSER = _browser
+            return
+
+        except WebDriverException:
+            continue
+
+        except Exception:
+            continue
+
+    errMsg = "You need to install a selenium webdriver first. "
+    errMsg += "(visit 'https://www.seleniumhq.org/docs/03_webdriver.jsp')"
+    logger.error(errMsg)
+
+    raise SystemExit
+
+def reinitWebDriver(reload_url=True):
+    """
+    Menginisialisasi ulang WebDriver
+    """
+
+    tryCloseWebDriver()
+
+    SETTING.BROWSER = SETTING.WEB_DRIVER()
+    if SETTING.HTTP_AUTH_HANDLER is None and TARGET.URL is not None:
+        if reload_url:
+            SETTING.BROWSER.get(TARGET.URL)
+
+def tryCloseWebDriver():
+    """
+    Menutup WebDriver
+    """
+
+    if SETTING.BROWSER is not None:
+        try:
+            SETTING.BROWSER.quit()
+        except:
+            pass
